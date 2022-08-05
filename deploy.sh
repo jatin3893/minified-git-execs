95a4e1dc19408f49748844de22f9fb78b4772871
Jatin Parekh <jatinparekh93@gmail.com>
2022-08-05 15:00:46 +0530

    Updated git binaries

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 rm -r compressed
 mkdir compressed
 tar -czvf compressed/dugite-native-v2.35.3-f7287d1-macOS-arm64.tar.gz extracted/dugite-native-v2.35.3-f7287d1-macOS-arm64
-tar -czvf compressed/dugite-native-v2.35.3-f7287d1-macOS-x64 extracted/dugite-native-v2.35.3-f7287d1-macOS-x64
-tar -czvf compressed/dugite-native-v2.35.3-f7287d1-ubuntu extracted/dugite-native-v2.35.3-f7287d1-ubuntu
-tar -czvf compressed/dugite-native-v2.35.3-f7287d1-windows-x64 extracted/dugite-native-v2.35.3-f7287d1-windows-x64
-tar -czvf compressed/dugite-native-v2.35.3-f7287d1-windows-x86 extracted/dugite-native-v2.35.3-f7287d1-windows-x86
+tar -czvf compressed/dugite-native-v2.35.3-f7287d1-macOS-x64.tar.gz extracted/dugite-native-v2.35.3-f7287d1-macOS-x64
+tar -czvf compressed/dugite-native-v2.35.3-f7287d1-ubuntu.tar.gz extracted/dugite-native-v2.35.3-f7287d1-ubuntu
+tar -czvf compressed/dugite-native-v2.35.3-f7287d1-windows-x64.tar.gz extracted/dugite-native-v2.35.3-f7287d1-windows-x64
+tar -czvf compressed/dugite-native-v2.35.3-f7287d1-windows-x86.tar.gz extracted/dugite-native-v2.35.3-f7287d1-windows-x86
 git add .
 git commit -m "Updated git binaries"
 git push
