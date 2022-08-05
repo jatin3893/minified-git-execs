842eb699b14cceb4573b0ec7603a8021084750cf
Jatin Parekh <jatinparekh93@gmail.com>
2022-08-06 00:21:08 +0530

    Updated builds

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ tar -czvf dugite-native-v2.35.3-f7287d1-ubuntu.tar.gz -C dugite-native-v2.35.3-f
 tar -czvf dugite-native-v2.35.3-f7287d1-windows-x64.tar.gz -C dugite-native-v2.35.3-f7287d1-windows-x64 .
 tar -czvf dugite-native-v2.35.3-f7287d1-windows-x86.tar.gz -C dugite-native-v2.35.3-f7287d1-windows-x86 .
 
+cd ../
 rm -r compressed
 mkdir compressed
 mv extracted/*.tar.gz compressed
