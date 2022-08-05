bfb41cf797d71d6969b9089cf6c986c2a8bd4a16
Jatin Parekh <jatinparekh93@gmail.com>
2022-08-06 00:20:02 +0530

    Temp commit

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,10 @@
 cd extracted
-tar -czvf dugite-native-v2.35.3-f7287d1-macOS-arm64.tar.gz dugite-native-v2.35.3-f7287d1-macOS-arm64
-tar -czvf dugite-native-v2.35.3-f7287d1-macOS-x64.tar.gz dugite-native-v2.35.3-f7287d1-macOS-x64
-tar -czvf dugite-native-v2.35.3-f7287d1-ubuntu.tar.gz dugite-native-v2.35.3-f7287d1-ubuntu
-tar -czvf dugite-native-v2.35.3-f7287d1-windows-x64.tar.gz dugite-native-v2.35.3-f7287d1-windows-x64
-tar -czvf dugite-native-v2.35.3-f7287d1-windows-x86.tar.gz dugite-native-v2.35.3-f7287d1-windows-x86
-
-cd ../
+tar -czvf dugite-native-v2.35.3-f7287d1-macOS-arm64.tar.gz -C dugite-native-v2.35.3-f7287d1-macOS-arm64 .
+tar -czvf dugite-native-v2.35.3-f7287d1-macOS-x64.tar.gz -C dugite-native-v2.35.3-f7287d1-macOS-x64 .
+tar -czvf dugite-native-v2.35.3-f7287d1-ubuntu.tar.gz -C dugite-native-v2.35.3-f7287d1-ubuntu .
+tar -czvf dugite-native-v2.35.3-f7287d1-windows-x64.tar.gz -C dugite-native-v2.35.3-f7287d1-windows-x64 .
+tar -czvf dugite-native-v2.35.3-f7287d1-windows-x86.tar.gz -C dugite-native-v2.35.3-f7287d1-windows-x86 .
 
 rm -r compressed
 mkdir compressed
 mv extracted/*.tar.gz compressed
-
-git add compressed
-git add .
-git commit -m "Updated git binaries"
-git push
