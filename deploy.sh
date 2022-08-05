bb773e8304ea041f5912ba83070fe3ba4eef8720
Jatin Parekh <jatinparekh93@gmail.com>
2022-08-05 17:20:01 +0530

    Updated git binaries

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
-rm -r compressed
-mkdir compressed
-
 cd extracted
 tar -czvf dugite-native-v2.35.3-f7287d1-macOS-arm64.tar.gz dugite-native-v2.35.3-f7287d1-macOS-arm64
 tar -czvf dugite-native-v2.35.3-f7287d1-macOS-x64.tar.gz dugite-native-v2.35.3-f7287d1-macOS-x64
@@ -9,9 +6,11 @@ tar -czvf dugite-native-v2.35.3-f7287d1-windows-x64.tar.gz dugite-native-v2.35.3
 tar -czvf dugite-native-v2.35.3-f7287d1-windows-x86.tar.gz dugite-native-v2.35.3-f7287d1-windows-x86
 
 cd ../
-mv extracted/*.tar.gz compressed
 
 git checkout gh-pages
+rm -r compressed
+mkdir compressed
+mv extracted/*.tar.gz compressed
 git add compressed
 git add .
 git commit -m "Updated git binaries"
