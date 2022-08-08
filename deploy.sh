079a889989ba142d384743252c0d5c5b8858dfb1
Jatin Parekh <jatinparekh93@gmail.com>
2022-08-08 15:34:23 +0530

    Updated binaries

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 cd extracted
-tar -czvf dugite-native-v2.35.3-f7287d1-macOS-arm64.tar.gz -C dugite-native-v2.35.3-f7287d1-macOS-arm64 .
-tar -czvf dugite-native-v2.35.3-f7287d1-macOS-x64.tar.gz -C dugite-native-v2.35.3-f7287d1-macOS-x64 .
-tar -czvf dugite-native-v2.35.3-f7287d1-ubuntu.tar.gz -C dugite-native-v2.35.3-f7287d1-ubuntu .
-tar -czvf dugite-native-v2.35.3-f7287d1-windows-x64.tar.gz -C dugite-native-v2.35.3-f7287d1-windows-x64 .
-tar -czvf dugite-native-v2.35.3-f7287d1-windows-x86.tar.gz -C dugite-native-v2.35.3-f7287d1-windows-x86 .
+tar --disable-copyfile -czvf dugite-native-v2.35.3-f7287d1-macOS-arm64.tar.gz -C dugite-native-v2.35.3-f7287d1-macOS-arm64 .
+tar --disable-copyfile -czvf dugite-native-v2.35.3-f7287d1-macOS-x64.tar.gz -C dugite-native-v2.35.3-f7287d1-macOS-x64 .
+tar --disable-copyfile -czvf dugite-native-v2.35.3-f7287d1-ubuntu.tar.gz -C dugite-native-v2.35.3-f7287d1-ubuntu .
+tar --disable-copyfile -czvf dugite-native-v2.35.3-f7287d1-windows-x64.tar.gz -C dugite-native-v2.35.3-f7287d1-windows-x64 .
+tar --disable-copyfile -czvf dugite-native-v2.35.3-f7287d1-windows-x86.tar.gz -C dugite-native-v2.35.3-f7287d1-windows-x86 .
 
 cd ../
 rm -r compressed
